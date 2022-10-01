f0a48c717c4db5fee65d1aba44657ccb35aa7679
h1810126 <h1810126@nushigh.edu.sg>
2022-10-01 18:51:45 +0800

    made the extension detect amogi

--- dev/extension/popup.js
+++ dev/extension/popup.js
@@ -19,6 +19,23 @@ changeColor.addEventListener("click", async () => {
 // current page
 function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+
+    const amongus_regex = /a\W*m\W*o\W*(n\W*g\W*u\W*s|n\W*g|g\W*u\W*s|n\W*g\W*i|g\W*i)/gim;
+    const replacer = (match, offset, string) => {
+      return `<span style="border-style: solid; border-width: 3px; border-color: ${color}; border-radius: 2px;">${match}</span>`;
+    }
+    
+    // check function, returns new text
+    function filter_text(text) {
+      return text.replace(amongus_regex, replacer);
+    }
+
+    // create a treewalker to get all text in document
+    const tree = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+    while (tree.nextNode()) {
+      const node = tree.currentNode;
+      const text = node.nodeValue;
+      node.parentElement.innerHTML = filter_text(node.parentElement.innerHTML);
+    }
   });
 }
