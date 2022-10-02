9550e9171e2e1f6bb26c61c0aa1ae4d914da911d
Tran Duc Khang <79127891+KhangTran2005@users.noreply.github.com>
2022-10-02 10:50:37 +0800

    asda

--- ai/cnn/isSlur.py
+++ ai/cnn/isSlur.py
@@ -18,7 +18,7 @@ def word2img(rw, rh, txt):
 
 def isSlur(word, threshold=0.7):
     if not os.path.exists("./isSlurModelv2"):
-        url = 'https://drive.google.com/file/d/1FVagAXCV27rGNSCB1uzpSVKqIQ4B5t6p/view?usp=sharing'
+        url = 'https://drive.google.com/file/d/1IQCTMrf_Kg3ERF1jfLbCgYw7m8rggjb4/view?usp=sharing'
         output = 'isSlurModelv3'
         gdown.download(url, output, quiet=True)
     model = pickle.load(open('isSlurModelv3', 'rb'))
